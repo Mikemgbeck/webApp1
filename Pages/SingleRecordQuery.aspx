d546aa2a9efbda21351e2e037e2024063ddbedd8
Michael Beck <mbeck6@nait.ca>
2020-06-27 20:20:24 -0600

    commit

--- Pages/SingleRecordQuery.aspx
+++ Pages/SingleRecordQuery.aspx
@@ -1,4 +1,22 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="SingleRecordQuery.aspx.cs" Inherits="webApp1.Pages.SingleRecordQuery" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
-    <h1>Hey Man</h1>
+    <h1>Simple Query by Primary Key (EX06)</h1>
+    <div class="row">
+        <div class="col-md-6">
+            <asp:Label runat="server" Text="Enter a TeamID:"></asp:Label>&nbsp;&nbsp;
+            <asp:TextBox ID="IDArg" runat="server"></asp:TextBox>&nbsp;&nbsp;
+            <asp:Button ID="Fetch" runat="server" Text="Fetch" OnClick="Fetch_Click" />
+            <asp:Label ID="Messagelabel" runat="server"></asp:Label>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <asp:Label runat="server" Text="Team ID:"></asp:Label>&nbsp;&nbsp;
+            <asp:Label id="ID" runat="server"></asp:Label>
+            <br />
+            <asp:Label runat="server" Text="Team Name:"></asp:Label>&nbsp;&nbsp;
+            <asp:Label ID="Name" runat="server"></asp:Label>
+        </div>
+    </div>
+
 </asp:Content>
--- Pages/SingleRecordQuery.aspx.cs
+++ Pages/SingleRecordQuery.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using DBSystem.BLL;
+using DBSystem.ENTITIES;
 
 namespace webApp1.Pages
 {
@@ -11,7 +13,53 @@ namespace webApp1.Pages
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            ID.Text = "";
+            Name.Text = "";
+        }
+        protected void Fetch_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(IDArg.Text))
+            {
+                Messagelabel.Text = "Enter a ID value";
+                ID.Text = "";
+                Name.Text = "";
+            } 
+            else
+            {
+                int id = 0;
+                if (int.TryParse(IDArg.Text, out id))
+                {
+                    if (id > 0)
+                    {
+                        Controller01 sysmgr = new Controller01();
+                        Entity01 info = null;
+                        info = sysmgr.FindByID(id);
+                        if (info == null)
+                        {
+                            Messagelabel.Text = "Id not found";
+                            ID.Text = "";
+                            Name.Text = "";
+                        }
+                        else
+                        {
+                            ID.Text = info.TeamID.ToString();
+                            Name.Text = info.TeamName;
+                        }
+                    }
+                    else
+                    {
+                        Messagelabel.Text = "Id must be greater than 0";
+                        ID.Text = "";
+                        Name.Text = "";
+                    }
+                }
+                else
+                {
+                    Messagelabel.Text = "Id must be a number";
+                    ID.Text = "";
+                    Name.Text = "";
+                }
+            }
         }
     }
 }
